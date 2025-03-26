b703e48d8020719c616704dd1cd173b000a7fdb2
suhailzap <suhailsap69@gmail.com>
2025-03-26 07:29:38 +0300

    Update NumericController.java

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -3,38 +3,46 @@ package com.devsecops;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/") // Base mapping for all endpoints
 public class NumericController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(NumericController.class);
+	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+	private static final String BASEURL = "http://node-service:5000/plusone";
+	
+	RestTemplate restTemplate = new RestTemplate();
+	
+	@RestController
+	public class compare {
 
-    private static final String BASE_URL = "http://node-service:5000/plusone"; // Fixed naming
+		@GetMapping("/")
+		public String welcome() {
+			return "Kubernetes DevSecOps";
+		}
 
-    private final RestTemplate restTemplate = new RestTemplate();
+		@GetMapping("/compare/{value}")
+		public String compareToFifty(@PathVariable int value) {
+			String message = "Could not determine comparison";
+			if (value > 50) {
+				message = "Greater than 50";
+			} else {
+				message = "Smaller than or equal to 50";
+			}
+			return message;
+		}
 
-    @GetMapping
-    public String welcome() {
-        return "Kubernetes DevSecOps";
-    }
+		@GetMapping("/increment/{value}")
+		public int increment(@PathVariable int value) {
+			ResponseEntity<String> responseEntity = restTemplate.getForEntity(baseURL + '/' + value, String.class);
+			String response = responseEntity.getBody();
+			logger.info("Value Received in Request - " + value);
+			logger.info("Node Service Response - " + response);
+			return Integer.parseInt(response);
+		}
+	}
 
-    @GetMapping("/compare/{value}")
-    public String compareToFifty(@PathVariable int value) {
-        return (value > 50) ? "Greater than 50" : "Smaller than or equal to 50";
-    }
-
-    @GetMapping("/increment/{value}")
-    public int increment(@PathVariable int value) {
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(BASE_URL + '/' + value, String.class);
-        String response = responseEntity.getBody();
-
-        LOGGER.info("Value Received in Request - {}", value);
-        LOGGER.info("Node Service Response - {}", response);
-
-        return Integer.parseInt(response);
-    }
 }
